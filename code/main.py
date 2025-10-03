04aa830b0939d5252c9ccd10a38d746372a8fb68
gits-squishy <darshanm0309@gmail.com>
2025-10-03 23:01:12 +0530

    third commit

--- code/main.py
+++ code/main.py
@@ -2,19 +2,24 @@ import cv2
 from cv2 import waitKey
 
 faceDetector  = cv2.CascadeClassifier('/home/ghost/Coding/detection/assets/haarcascade_frontalface_default.xml')
+eyeDetector = cv2.CascadeClassifier('/home/ghost/Coding/detection/assets/haarcascade_eye.xml')
 
 video = cv2.VideoCapture(0)
 
 while True:
     val , frame = video.read()
-    videoData = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
-    videoData = cv2.equalizeHist(videoData)
-    detectedFace = faceDetector.detectMultiScale(videoData , scaleFactor=1.02, minNeighbors=6, minSize=(60,60))
+    grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.equalizeHist(grayFrame)
+    grayFrame = cv2.GaussianBlur(grayFrame , (5,5) , 0)
+    detectedFace = faceDetector.detectMultiScale(grayFrame , scaleFactor=1.02, minNeighbors=6, minSize=(60,60))
+    detectedEyes = eyeDetector.detectMultiScale(grayFrame, scaleFactor=1.1, minNeighbors=8,minSize=(30,30) , maxSize=(80,80))
+
 
     for face in detectedFace:
         x, y, w, h = face
         cv2.rectangle(frame, (x,y),(x+w , y+ h) ,(0,255,0),2)
-
+    for (x,y, w, h) in detectedEyes:
+        cv2.rectangle(frame, (x,y) , (x+w , y+h), (0,0,255),thickness=1)
 
     cv2.imshow("Frame" , frame)
     cv2.resizeWindow("Frame" , 1280,720)
