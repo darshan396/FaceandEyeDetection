870c0ab0de74f64def6019eaa8888255bea6359e
gits-squishy <darshanm0309@gmail.com>
2025-10-03 07:44:20 +0530

    second commit

--- code/main.py
+++ code/main.py
@@ -1,15 +1,22 @@
 import cv2
 from cv2 import waitKey
 
-faceDetector  = cv2.CascadeClassifier('assets/haarcascade_frontalface_default.xml')
+faceDetector  = cv2.CascadeClassifier('/home/ghost/Coding/detection/assets/haarcascade_frontalface_default.xml')
 
 video = cv2.VideoCapture(0)
 
 while True:
     val , frame = video.read()
     videoData = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
-    detectedFace = faceDetector.detectMultiScale(videoData , scaleFactor=1.07, minNeighbors=10, minSize=(45,45))
-    cv2.imshow("Frame" , videoData)
+    videoData = cv2.equalizeHist(videoData)
+    detectedFace = faceDetector.detectMultiScale(videoData , scaleFactor=1.02, minNeighbors=6, minSize=(60,60))
+
+    for face in detectedFace:
+        x, y, w, h = face
+        cv2.rectangle(frame, (x,y),(x+w , y+ h) ,(0,255,0),2)
+
+
+    cv2.imshow("Frame" , frame)
     cv2.resizeWindow("Frame" , 1280,720)
     if waitKey(60) & 0xFF == ord('q'):
         break
