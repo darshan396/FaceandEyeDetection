8b5a20895d8298135df9ea0bf91fbde8bf9332b2
gits-squishy <darshanm0309@gmail.com>
2025-10-07 21:54:04 +0530

    image capture commit

--- code/main.py
+++ code/main.py
@@ -1,11 +1,13 @@
 import cv2
 from cv2 import waitKey
+import os
 
 faceDetector  = cv2.CascadeClassifier('/home/ghost/Coding/detection/assets/haarcascade_frontalface_default.xml')
 eyeDetector = cv2.CascadeClassifier('/home/ghost/Coding/detection/assets/haarcascade_eye.xml')
 
 video = cv2.VideoCapture(0)
-
+savedImagePath = os.path.join(".." , "pics")
+count = 0
 while True:
     val , frame = video.read()
     grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -14,16 +16,27 @@ while True:
     detectedFace = faceDetector.detectMultiScale(grayFrame , scaleFactor=1.02, minNeighbors=6, minSize=(60,60))
     detectedEyes = eyeDetector.detectMultiScale(grayFrame, scaleFactor=1.1, minNeighbors=8,minSize=(30,30) , maxSize=(80,80))
 
-
     for face in detectedFace:
         x, y, w, h = face
-        cv2.rectangle(frame, (x,y),(x+w , y+ h) ,(0,255,0),2)
+        cv2.rectangle(frame, (x,y),(x+w , y+h) ,(0,255,0),2)
     for (x,y, w, h) in detectedEyes:
         cv2.rectangle(frame, (x,y) , (x+w , y+h), (0,0,255),thickness=1)
+    filename = os.path.join(savedImagePath , f"image_{count}.png")
+    count += 1
 
     cv2.imshow("Frame" , frame)
     cv2.resizeWindow("Frame" , 1280,720)
-    if waitKey(60) & 0xFF == ord('q'):
+    if cv2.waitKey(25) & 0xFF == ord('c'):
+        filename = os.path.join(savedImagePath, f"image_{count}.png")
+
+        for face in detectedFace:
+            x , y , w,  h = face
+            detFace = frame[ y:y+h , x:x+h]
+            cv2.imwrite(filename , detFace)
+            print("Saved!")
+            count += 1
+
+    if waitKey(25) & 0xFF == ord('q'):
         break
 
 video.release()
